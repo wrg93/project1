dd3ce3d2255114b744123252f256a8ad261ea125
William Grundler <wrgrundler@gmail.com>
2020-05-04 20:36:22 -0700

    function added which only allows you to enter numeric value in dependents input

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-var dependentsInput = document.createElement("TEXTAREA");
+//creating Input in which user enters number of dependents
+var dependentsInput = document.createElement("INPUT");
 document.body.appendChild(dependentsInput);
+dependentsInput.setAttribute("type", "text");
+dependentsInput.setAttribute("class", "only-numeric");
+
+
+//creating errorSpan
+var errorSpan = document.createElement("span");
+errorSpan.setAttribute("class", "error");
+errorSpan.textContent="Error. Please input digits only";
+
+//function which will not allow you to enter values other than numbers
+$(document).ready(function() {
+    $(".only-numeric").bind("keypress", function (e) {
+        var keyCode = e.which ? e.which : e.keyCode
+             
+        if (!(keyCode >= 48 && keyCode <= 57)) {
+          $(".error").css("display", "inline");
+          return false;
+        }else{
+          $(".error").css("display", "none");
+        }
+    });
+  });
