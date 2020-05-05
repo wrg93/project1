1e30007676aa7aa4905189109804b41e269eeaca
William Grundler <wrgrundler@gmail.com>
2020-05-04 23:35:48 -0700

    Changed to selectors

--- script.js
+++ script.js
@@ -1,55 +1,65 @@
-//creating Input in which user enters number of dependents
-var dependentsInput = document.createElement("INPUT");
-document.body.appendChild(dependentsInput);
-dependentsInput.setAttribute("type", "text");
-dependentsInput.setAttribute("class", "only-numeric");
+//Selector for Number of dependents
+var numberSelector = document.createElement("SELECT");
+numberSelector.setAttribute("id", "mySelect");
+document.body.appendChild(numberSelector);
 
+var optionOne = document.createElement("OPTION");
+optionOne.setAttribute("value", "optionOne");
+optionOne.textContent="1";
+numberSelector.appendChild(optionOne);
 
-//creating errorSpan
-var errorSpan = document.createElement("span");
-errorSpan.setAttribute("class", "error");
-errorSpan.textContent="Error. Please input digits only";
+var optionTwo = document.createElement("OPTION");
+optionTwo.setAttribute("value", "optionTwo");
+optionTwo.textContent="2";
+numberSelector.appendChild(optionTwo);
 
-//function which will not allow you to enter values other than numbers
-$(document).ready(function() {
-    $(".only-numeric").bind("keypress", function (e) {
-        var keyCode = e.which ? e.which : e.keyCode
-             
-        if (!(keyCode >= 48 && keyCode <= 57)) {
-          $(".error").css("display", "inline");
-          return false;
-        }else{
-          $(".error").css("display", "none");
-        }
-    });
-});
+var optionThree = document.createElement("OPTION");
+optionThree.setAttribute("value", "optionThree");
+optionThree.textContent="3";
+numberSelector.appendChild(optionThree);
 
+var optionFour = document.createElement("OPTION");
+optionFour.setAttribute("value", "optionFour");
+optionFour.textContent="4";
+numberSelector.appendChild(optionFour);
 
-//creating radio buttons confirming if user or dependents are handicapped
-var radioForm = document.createElement("FORM");
-document.body.appendChild(radioForm);
+var optionFive = document.createElement("OPTION");
+optionFive.setAttribute("value", "optionFive");
+optionFive.textContent="5";
+numberSelector.appendChild(optionFive);
 
-var yesRadio = document.createElement("INPUT");
-yesRadio.setAttribute("type", "radio");
-yesRadio.setAttribute("id","yesRadio");
-yesRadio.setAttribute("name","radio")
-yesRadio.setAttribute("value","yes");
-radioForm.appendChild(yesRadio);
+var optionSix = document.createElement("OPTION");
+optionSix.setAttribute("value", "optionSix");
+optionSix.textContent="6";
+numberSelector.appendChild(optionSix);
 
-var yesRadioLabel = document.createElement("LABEL");
-yesRadioLabel.setAttribute("for","yes");
-yesRadioLabel.textContent="Yes";
-yesRadio.appendChild(yesRadioLabel);
+var optionSeven = document.createElement("OPTION");
+optionSeven.setAttribute("value", "optionSeven");
+optionSeven.textContent="7";
+numberSelector.appendChild(optionSeven);
 
+var optionEight = document.createElement("OPTION");
+optionEight.setAttribute("value", "optionEight");
+optionEight.textContent="8";
+numberSelector.appendChild(optionEight);
 
-var noRadio = document.createElement("INPUT");
-noRadio.setAttribute("type", "radio");
-noRadio.setAttribute("id","noRadio");
-noRadio.setAttribute("name","radio");
-noRadio.setAttribute("value","no");
-radioForm.appendChild(noRadio);
+var optionNine = document.createElement("OPTION");
+optionNine.setAttribute("value", "optionNine");
+optionNine.textContent="9";
+numberSelector.appendChild(optionNine);
 
-var noRadioLabel = document.createElement("LABEL");
-noRadioLabel.setAttribute("for","noRadio");
-noRadioLabel.textContent="No";
-noRadio.appendChild(noRadioLabel);
+//Selector for disabilities
+var disabilities = document.createElement("SELECT");
+disabilities.setAttribute("id", "disabilities");
+document.body.appendChild(disabilities);
+
+var optionYes = document.createElement("OPTION");
+optionYes.setAttribute("value", "optionYes");
+optionYes.textContent="Yes";
+disabilities.appendChild(optionYes);
+
+var optionNo = document.createElement("OPTION");
+optionNo.setAttribute("value", "optionNo");
+optionNo.textContent="No";
+disabilities.appendChild(optionNo);
+
